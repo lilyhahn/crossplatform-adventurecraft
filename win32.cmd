911f4c1bad11b3c8c8705e7cc8290b1d0b555295
Toby Hahn <thbike17@gmail.com>
2013-01-26 18:07:20 -0500

    alright, actually fixed windows!

--- win32.cmd
+++ win32.cmd
@@ -1,7 +1,7 @@
-cd %~dp0\.minecraft\bin\
+cd %~dp0.minecraft\bin\
 del natives /S
 xcopy natives_win natives
-cd %~dp0\swt\
-copy swt_win32.jar %~dp0\swt.jar
+cd %~dp0swt\
+copy swt_win32.jar %~dp0swt.jar
 cd %~dp0\
 AdventureCraft
